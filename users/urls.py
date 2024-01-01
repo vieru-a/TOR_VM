dda604e638cbf09ea388381ee1e1b5025fa6738b
vieru-a <vierua2200@gmail.com>
2024-01-01 21:42:59 +0300

    добавлена восстановление пароля через почту

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,5 @@
-from django.contrib.auth.views import LogoutView, PasswordChangeDoneView
+from django.contrib.auth.views import LogoutView, PasswordChangeDoneView, PasswordResetView, PasswordResetDoneView, \
+    PasswordResetConfirmView, PasswordResetCompleteView
 from django.urls import path
 from .views import *
 
@@ -10,5 +11,27 @@ urlpatterns = [
     path('register/', RegisterUser.as_view(), name='register'),
     path('profile/', ProfileUser.as_view(), name='profile'),
     path('password_change/', PasswordChangeUser.as_view(), name='password_change'),
-    path('password_change/done/', PasswordChangeDoneView.as_view(template_name='users/password_change_done.html'), name='password_change_done'),
+
+    path('password_change/done/',
+         PasswordChangeDoneView.as_view(template_name='users/password_change_done.html'),
+         name='password_change_done'),
+
+    path('password_reset/',
+         PasswordResetView.as_view(template_name='users/password_reset_form.html',
+                                   email_template_name='users/password_reset_email.html',
+                                   success_url=reverse_lazy('users:password_reset_done')),
+         name='password_reset'),
+
+    path('password_reset/done/',
+         PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),
+         name='password_reset_done'),
+
+    path('password_reset/<uidb64>/<token>/',
+         PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html',
+                                          success_url=reverse_lazy('users:password_reset_complete')),
+         name='password_reset_confirm'),
+
+    path('password_reset/complete/',
+         PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),
+         name='password_reset_complete'),
 ]
