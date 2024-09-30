c731ff0c064c7d332ebb7e560f54774c03a6183d
vieru-a <vierua2200@gmail.com>
2024-09-30 18:37:14 +0300

    изменил импорт модели Feedback

--- feedback/forms.py
+++ feedback/forms.py
@@ -1,12 +1,11 @@
 from django import forms
 from django.forms import Textarea
 
-from feedback.models import Feedback
+from .models import Feedback
 
 
 class FeedbackForm(forms.ModelForm):
 
-    # текст для плейсхолдера
     class Meta:
         model = Feedback
         fields = ['first_name', 'last_name', 'email', 'message']
