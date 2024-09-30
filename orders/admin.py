bb7655c811be1be2d41778eba094e5fcbd4e80b7
vieru-a <vierua2200@gmail.com>
2024-09-30 18:30:15 +0300

    добавил поле user в заказах в админке

--- orders/admin.py
+++ orders/admin.py
@@ -9,8 +9,8 @@ class OrderItemInline(admin.TabularInline):
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ['id', 'email', 'phone_number', 'first_name', 'last_name', 'city', 'paid']
-    list_display_links = ('id', 'email')
+    list_display = ['id', 'email', 'phone_number', 'first_name', 'last_name', 'user', 'paid']
+    list_display_links = ('id', 'email', 'user')
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
     search_fields = ('email', 'phone_number', 'first_name', 'last_name')
